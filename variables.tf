c689f8e953335dd017af2b8277be52b10e598e6f
santoshjessey <107042702+santoshjessey@users.noreply.github.com>
2022-08-18 16:53:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" { default = "us-east-2" }
-variable "environment" { default = "" }
+variable "environment" { default = "test" }
 variable "project" { default = "javaweb" }
 variable "platform" { default = "Devops" }
 variable "vpc_id" { default = "vpc-0e1f30f6c6aa665f8" }
